a74a55629ff6a2f4134e548d32ad5b7c828f0aac
ginni-1 <117846881+ginni-1@users.noreply.github.com>
2025-04-02 12:11:49 +0530

    first commit

---
+++ constants/constants.js
@@ -0,0 +1,67 @@
+export const NAME = 'Darshika verma';
+
+export const DESIGNATION = `MERN Stack Developer | Open Source Contributor | Tech Blogger`;
+
+export const DETAILS = {
+  Residence: 'india',
+  City: 'bijnor',
+  Age: '21',
+};
+
+export const SKILLS = [
+  {
+    title: 'MERN Stack Developer',
+    level: '75%',
+  },
+  {
+    title: 'React Developer',
+    level: '91%',
+  },
+  // {
+  //   title: 'Desktop Application Developer',
+  //   level: '75%',
+  // },
+  {
+    title: 'Backend Developer',
+    level: '54%',
+  },
+  {
+    title: 'Technical Blogger',
+    level: '83%',
+  },
+];
+
+export const TECH_STACK = [
+  'JavaScript',
+  'ReactJS',
+  'NextJS',
+  'TypeScript',
+  'NodeJS',
+  'Redux',
+  'Zustand',
+  'CSS',
+  'SCSS',
+  'TailwindCSS',
+  'Ant Design',
+  'Material UI',
+  'Next UI',
+  'REST API',
+  'MongoDB',
+  'MySQL',
+  'Git',
+  'GitHub',
+  'Jira',
+  'Figma',
+  'Adobe XD',
+];
+
+export const CONTACTS = {
+  EMAIL: 'ginniv2003@gmail.com',
+  PHONE: '+923159591656',
+};
+export const SOCIAL_LINKS = {
+  GITHUB: 'https://github.com/',
+  LINKEDIN: 'https://www.linkedin.com/in//',
+  TWITTER: 'https://twitter.com/',
+  FACEBOOK: 'https://www.facebook.com/',
+};
